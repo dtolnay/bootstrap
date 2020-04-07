b11b4bfec3f7a6d4f8f43c11f5b54d756b125017
David Tolnay <dtolnay@gmail.com>
2020-04-06 20:58:01 -0700

    Wrap long curl lines

--- download.sh
+++ download.sh
@@ -8,9 +8,13 @@ mkdir -p sources
 cd sources
 
 echo "Downloading mrustc-${mrustc_version}"
-curl -L "https://github.com/thepowersgang/mrustc/archive/v${mrustc_version}.tar.gz" -o "mrustc-${mrustc_version}.tar.gz"
+curl \
+    -L "https://github.com/thepowersgang/mrustc/archive/v${mrustc_version}.tar.gz" \
+    -o "mrustc-${mrustc_version}.tar.gz"
 
 for v in "${mrustc_rustc_version}" "${rustc_versions[@]}"; do
     echo "Downloading rustc-$v"
-    curl -L "https://static.rust-lang.org/dist/rustc-$v-src.tar.gz" -o "rustc-$v-src.tar.gz"
+    curl \
+        -L "https://static.rust-lang.org/dist/rustc-$v-src.tar.gz" \
+        -o "rustc-$v-src.tar.gz"
 done
