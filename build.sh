75cd9486f2f6bf19a83e53d06acb0f6c0834d1b9
David Tolnay <dtolnay@gmail.com>
2023-10-15 20:33:35 -0700

    Update to mrustc 0.10.1

--- build.sh
+++ build.sh
@@ -4,20 +4,18 @@ set -euo pipefail
 
 source /build/versions.sh
 
-cd "/build/rustc-${rustc_versions[0]}"
-echo "Patching rustc ${rustc_versions[0]}"
-patch -p0 <"../mrustc-${mrustc_version}/rustc-1.29.0-src.patch"
-
 cd "/build/mrustc-${mrustc_version}"
 export LIBSSH2_SYS_USE_PKG_CONFIG=true
-export MINICARGO_FLAGS="-j$(nproc)"
+export PARLEVEL="$(nproc)"
+export RUSTC_VERSION="${rustc_versions[0]}"
 echo "Building mrustc ${mrustc_version}"
 make -j$(nproc) -f minicargo.mk bin/mrustc
 echo "Building rustc ${rustc_versions[0]}"
-make -j$(nproc) -f minicargo.mk RUSTCSRC="../rustc-${rustc_versions[0]}/" output/rustc
-make -j$(nproc) -f minicargo.mk RUSTCSRC="../rustc-${rustc_versions[0]}/" output/cargo
+make -j$(nproc) -f minicargo.mk RUSTCSRC
+make -j$(nproc) -f minicargo.mk output/rustc
+make -j$(nproc) -f minicargo.mk output/cargo
 cd "/build/mrustc-${mrustc_version}/run_rustc"
-make -j$(nproc) RUST_SRC="../../rustc-${rustc_versions[0]}/src/"
+make -j$(nproc)
 
 export CARGO_HOME=/build/.cargo
 unset SUDO_USER
