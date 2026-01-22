9571c097d26d9bf11e35b6d22d916eb8821779fd
David Tolnay <dtolnay@gmail.com>
2026-01-22 09:34:05 -0800

    Add rustc 1.93.0

--- versions.sh
+++ versions.sh
@@ -20,4 +20,5 @@ rustc_checksum[1.89.0]=2576f9f440dd99b0151bd28f59aa0ac6102d5c4f3ed4ef8a810c8dd05
 rustc_checksum[1.90.0]=799a9f9cba4ed5351e071048bcf6b5560755d9009648def33a407dd4961f9b7e
 rustc_checksum[1.91.1]=38dce205d39f61571261f0444237a1ce9efecb970e760d8ec4d957af5b445723
 rustc_checksum[1.92.0]=9e0d2ca75c7e275fdc758255bf4b03afb3d65d1543602746907c933b6901c3b8
+rustc_checksum[1.93.0]=69112bd83c321943ffc390b7db2f59ef3febb95dac700f67c2156fbf6b50a705
 rustc_versions=($(printf "%s\n" "${!rustc_checksum[@]}" | sort -V))
